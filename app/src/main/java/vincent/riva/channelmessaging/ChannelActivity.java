5b225f8fe1852fec89711e17e8519b3bb4f5bf2f
Vincent RIVA <rivav@iut-acy.local>
2017-01-23 15:40:04 +0100

    Add Send Message Feature

--- app/src/main/java/vincent/riva/channelmessaging/ChannelActivity.java
+++ app/src/main/java/vincent/riva/channelmessaging/ChannelActivity.java
@@ -1,35 +1,82 @@
 package vincent.riva.channelmessaging;
 
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.gson.Gson;
 
 public class ChannelActivity extends AppCompatActivity {
 
+    private String accesstoken;
+    private int channelID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channel);
-        int channelID = getIntent().getIntExtra("channelID", 1);
-        final ListView listViewMessages = (ListView)findViewById(R.id.listViewMessages);
+        this.channelID = getIntent().getIntExtra("channelID", 1);
 
         SharedPreferences settings = getSharedPreferences("MyPrefs", 0);
-        String token = settings.getString("accesstoken", "");
+        this.accesstoken = settings.getString("accesstoken", "");
 
-        AsyncTaskClass async = new AsyncTaskClass();
+        final EditText editTextMessage = (EditText)findViewById(R.id.editTextMessage);
+        Button buttonEnvoyer = (Button)findViewById(R.id.buttonEnvoyer);
 
-        async.setOnCompleteRequestListener(new OnCompleteRequestListener() {
+        buttonEnvoyer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AsyncTaskClass async2 = new AsyncTaskClass();
+                String message = editTextMessage.getText().toString();
+                editTextMessage.setText("");
+                async2.execute("http://www.raphaelbischof.fr/messaging/?function=sendmessage", "accesstoken", accesstoken, "channelid", channelID+"", "message", message);
+            }
+        });
+        final Handler handler = new Handler();
+
+        final Runnable r = new Runnable() {
+            public void run() {
+                refreshMessages();
+                handler.postDelayed(this, 1000);
+            }
+        };
+        r.run();
+    }
+
+    public void getUsers()
+    {
+    }
+
+    private void refreshMessages()
+    {
+        AsyncTaskClass asyncUsers = new AsyncTaskClass();
+
+        asyncUsers.setOnCompleteRequestListener(new OnCompleteRequestListener() {
             @Override
             public void onCompleteRequest(String response) {
                 Gson gson = new Gson();
-                ResponseMessageList messageList = gson.fromJson(response, ResponseMessageList.class);
-                listViewMessages.setAdapter(new MessageArrayAdapter(getApplicationContext(), messageList.getMessages()));
+                final ResponseUserList usersList = gson.fromJson(response, ResponseUserList.class);
+
+                final AsyncTaskClass async = new AsyncTaskClass();
+
+                async.setOnCompleteRequestListener(new OnCompleteRequestListener() {
+                    @Override
+                    public void onCompleteRequest(String response) {
+                        final ListView listViewMessages = (ListView)findViewById(R.id.listViewMessages);
+                        Gson gson = new Gson();
+                        ResponseMessageList messageList = gson.fromJson(response, ResponseMessageList.class);
+                        listViewMessages.setAdapter(new MessageArrayAdapter(getApplicationContext(), messageList.getMessages(), usersList));
+                    }
+                });
+                async.execute("http://www.raphaelbischof.fr/messaging/?function=getmessages", "accesstoken", accesstoken, "channelid", channelID+"");
             }
         });
 
-        async.execute("http://www.raphaelbischof.fr/messaging/?function=getmessages", "accesstoken", token, "channelid", channelID+"");
+        asyncUsers.execute("http://www.raphaelbischof.fr/messaging/?function=getchannelusers", "accesstoken", this.accesstoken, "channelid", this.channelID+"");
     }
 }
--- app/src/main/java/vincent/riva/channelmessaging/MessageArrayAdapter.java
+++ app/src/main/java/vincent/riva/channelmessaging/MessageArrayAdapter.java
@@ -1,21 +1,35 @@
 package vincent.riva.channelmessaging;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MessageArrayAdapter extends ArrayAdapter<ResponseMessage> {
 
     private final Context context;
+    private ResponseUserList users;
 
-    public MessageArrayAdapter(Context context, List<ResponseMessage> objects) {
+    public MessageArrayAdapter(Context context, List<ResponseMessage> objects, ResponseUserList users) {
         super(context, R.layout.message_layout, objects);
         this.context = context;
+        this.users = users;
     }
 
     @Override
@@ -26,7 +40,11 @@ public class MessageArrayAdapter extends ArrayAdapter<ResponseMessage> {
         TextView textViewUser = (TextView)rowView.findViewById(R.id.textViewUser);
         TextView textViewDate = (TextView)rowView.findViewById(R.id.textViewDate);
         TextView textViewMessage = (TextView)rowView.findViewById(R.id.textViewMessage);
-        textViewUser.setText(message.getUserID()+"");
+        ImageView imageViewAvatar = (ImageView)rowView.findViewById(R.id.imageViewAvatar);
+
+        ResponseUser u = this.users.get(message.getUserID());
+
+        textViewUser.setText(u.getIdentifiant());
         textViewDate.setText(message.getData());
         textViewMessage.setText(message.getMessage());
         return rowView;
